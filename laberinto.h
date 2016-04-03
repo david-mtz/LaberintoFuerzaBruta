4c113c80343cceb5c49f4e9760ffee8ea48af0a1
david-mtz <martinez.david.g.a@gmail.com>
2016-04-02 19:35:55 -0600

    Cabecera
    Iniciar laberinto.h

---
+++ laberinto.h
@@ -0,0 +1,12 @@
+#ifndef _LABERINTO_H_
+#define _LABERINTO_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define archivoSol "soluciones.txt" // Archivo de salida de las soluciones. (aqui se almacenan las soluciones del laberinto)
+
+/* DECLARACION DE FUNCIONES, ESTRUCTURAS, ETC */
+/* ARCHIVO DE INFORMACION DE CADA FUNCION */
+
+#endif
