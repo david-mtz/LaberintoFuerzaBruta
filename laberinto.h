2989f084b67ce4db4501140dd0d94070a9cf939c
david-mtz <martinez.david.g.a@gmail.com>
2016-04-03 12:19:38 -0500

    Declaracion de nuevas funciones

--- laberinto.h
+++ laberinto.h
@@ -4,13 +4,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define archivoSol "soluciones.txt" // Archivo de salida de las soluciones. (aqui se almacenan las soluciones del laberinto)
+#define archivoSol "soluciones.txt" // Archivo de salida de las soluciones.
+
+// Algoritmo que resuelve un laberinto, metodo de la mano derecha con backtracking,
+// Camino = 0
+// Paredes = 1
 
 // Estructura que identifica las coordenadas.
 typedef struct coordenadas {
 	int renglon; // Posicion en renglon (eje y)
 	int columna; // Posicion en columna (eje x)
 } coord;
+
+// Estructura para trabajar un arreglo dinamico y que almacena las coordenadas.
+typedef struct pilaDin {
+	int nElementos; // # de elementos.
+	coord *elementos; // Arreglo
+} pilaD;
 
 // Estructura principal, especifica para el laberinto.
 typedef struct structLab {
@@ -40,7 +50,12 @@ laberinto genLab(char const *nombreArc, int iRenglon, int iColumna);
 void imprimirL(laberinto nuevoL);
 // Funcion que libera las estructuras y datos del laberinto.
 void freeL(laberinto freeL);
-
-
+/* Resolucion de laberinto */
+// Funcion que devuelve la posible celda para el siguiente movimiento.
+coord posibleCelda(laberinto nuevoL, coord celdaActual, pilaD* visitada);
+// Funcion que recorre un arreglo dinamico en busca de una coordenada.
+int buscarCelda(pilaD* visitada, coord celdaActual);
+// Funcion que resuelve el laberinto.
+void resolverL(laberinto nuevoL, int pasos);
+/* Fin Laberinto */
 #endif
-
