addb48eab8a40b4ff970df5b1142c4cc9bf318ad
david-mtz <martinez.david.g.a@gmail.com>
2016-04-03 12:21:21 -0500

    Uso de las nuevas funciones.

--- main.c
+++ main.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "laberinto.h"
-/* laberinto.h -> contenido para resolver el laberinto */
 
 /* Recibir argumentos */
 int main(int argc, char const *argv[]) {
-	char pasoxpaso;
-	if(argc < 4 || argc >4 ){
-		printf("Introduce los parametros necesarios.\n");
-		printf("Ej.: laberinto.txt 1 1\n");
-		printf("Primer numero, coordenada  en x(columna) y segundo numero, coordenada en y(renglon).\n");
-		exit(EXIT_FAILURE);
-	}
+    char pasoxpaso;
+    if (argc < 4 || argc > 4) { // 4 Argumentos [archivo nom_laberinto x y]
+        printf("Introduce los parametros necesarios.\n");
+        printf("Ej.: laberinto.txt 1 1\n");
+        printf("Primer numero, coordenada  en x(columna) y segundo numero, coordenada en y(renglon).\n");
+        exit(EXIT_FAILURE);
+    }
 
-	laberinto nuevoL;
+    laberinto nuevoL;
 
-	nuevoL = genLab(argv[1], atoi(argv[2]), atoi(argv[3]));
+    nuevoL = genLab(argv[1], atoi(argv[2]), atoi(argv[3])); // Generar laberinto.
 
-	imprimirL(nuevoL);
-	
+    imprimirL(nuevoL); // Imprimir el laberinto.
+
+    printf("Deseas que se muestre paso a paso la resolucion? S = Si\n"); // Activar la funcion simularPaso.
+    pasoxpaso = getchar();
+
+    if (pasoxpaso == 's' || pasoxpaso == 'S')
+        resolverL(nuevoL, 1); // Activa la funcion simularPaso, aun no implementada.
+    else
+        resolverL(nuevoL, 0); // Modo normal, sin mostrar proceso paso a paso.
+
+    freeL(nuevoL);
     return 0;
 }
