196a6d876c1320a82df62a5501b8b27a2a3b6332
david-mtz <martinez.david.g.a@gmail.com>
2016-04-02 23:18:37 -0600

    Se agregaron las estructuras para el laberinto

--- laberinto.h
+++ laberinto.h
@@ -6,8 +6,21 @@
 
 #define archivoSol "soluciones.txt" // Archivo de salida de las soluciones. (aqui se almacenan las soluciones del laberinto)
 
-/* DECLARACION DE FUNCIONES, ESTRUCTURAS, ETC */
-/* ARCHIVO DE INFORMACION DE CADA FUNCION */
+// Estructura que identifica las coordenadas.
+typedef struct coordenadas {
+	int renglon; // Posicion en renglon (eje y)
+	int columna; // Posicion en columna (eje x)
+} coord;
+
+// Estructura principal, especifica para el laberinto.
+typedef struct structLab {
+	int **camino; // Almacenar el camino del laberinto.
+	int columnas; // # de columnas.
+	int renglones; // # de renglones.
+	int nSalidas; // # de salidas posibles.
+	coord inicio; // Coordenadas de inicio.
+
+} laberinto;
 
 #endif
 
